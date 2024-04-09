e969894d915ec58de076d848ad4dd918aad00064
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-04-09 08:32:25 +0200

    button id

--- script.js
+++ script.js
@@ -24,8 +24,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getDatabase();
+const db = getFirestore(app);
 const auth = getAuth(app);
 
 let EmailInput = document.getElementById('email');
